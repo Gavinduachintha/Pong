cd51fc4b4fbb99d4d822bcce6cec79f339457fc6
gavi <agavindu123@gmail.com>
2025-02-27 22:41:00 +0530

    Added 2nd padd for dual playing

--- app.py
+++ app.py
@@ -20,9 +20,13 @@ ball_speed_y=4
 #pad properties
 pad_height=100
 pad_width=20
-pad_x_position=0
-pad_y_position=0
-pad_speed=0
+left_pad_x_position=0
+left_pad_y_position=0
+right_pad_x_position=screen_height//2
+right_pad_y_position=680
+
+left_pad_speed=0
+right_pad_speed=0
 
 white=(255,255,255)
 
@@ -31,9 +35,9 @@ font = pygame.font.Font(None,50)
 
 def displayScore():
     global score
-    score+=1
     text=font.render(f"Score: {score}",True,(255,255,255))
-    screen.blit(text,(screen_width//2,10))
+    text_rect=text.get_rect(center=(screen_width//2,20))
+    screen.blit(text,text_rect)
 
     
 
@@ -49,53 +53,78 @@ while running:
         
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_w:
-                    pad_speed=-10
+                    left_pad_speed=-10
 
             elif event.key==pygame.K_s:
-                    pad_speed=10
+                    left_pad_speed=10
+            
+            elif event.key==pygame.K_DOWN:
+                 right_pad_speed=10
+            
+            elif event.key==pygame.K_UP:
+                 right_pad_speed=-10
+            
+            elif event.key==pygame.K_q:
+                 running=False
                     
         elif event.type==pygame.KEYUP:
-            if event.key==pygame.K_w or event.key==pygame.K_s:
-                pad_speed=0
+            if event.key==pygame.K_w or event.key==pygame.K_s: 
+                left_pad_speed=0
+            if event.key==pygame.K_UP or event.key==pygame.K_DOWN:
+                 right_pad_speed=0
         
-        if pad_x_position<0:
-            pad_x_position=0 
-    pad_x_position+=pad_speed
- 
+        
+    left_pad_x_position+=left_pad_speed
+    left_pad_x_position=max(0,min(screen_height-pad_height,left_pad_x_position))
+
+    right_pad_x_position+=right_pad_speed
+    right_pad_x_position=max(0,min(screen_height-pad_height,right_pad_x_position))
             
-                            
+    #Move the ball
     ball_x_position+=ball_speed_x
     ball_y_position+=ball_speed_y
     
-    if(ball_x_position<=ball_radius or ball_x_position>=screen_width-ball_radius):
-        ball_speed_x=-ball_speed_x
-        
+    #Ball collision with the left and right boundaries
+    # if(ball_x_position<ball_radius or ball_x_position>=screen_width-ball_radius   ):
+    #     ball_speed_x=-ball_speed_x
 
+    
+    #Balll collision with the bottom and top boundaries
     if(ball_y_position<=ball_radius or ball_y_position>=screen_height-ball_radius):
         ball_speed_y=-ball_speed_y
         
-    if (ball_x_position-ball_radius) <= pad_width and pad_x_position < ball_y_position < pad_x_position + pad_height:
+    #Ball collision with the paddel
+    if (ball_x_position-ball_radius) <= pad_width and left_pad_x_position < ball_y_position < left_pad_x_position + pad_height:
         ball_speed_x = -ball_speed_x
         hit = True
 
-    if(ball_x_position-ball_radius<=0):
+    if (ball_x_position-ball_radius) <= pad_width and right_pad_x_position < ball_y_position < right_pad_x_position + pad_height:
+        ball_speed_x=-ball_speed_x
+        hit = True
+
+    #Paddle missing and game restarting
+    if(ball_x_position-ball_radius<=0 or ball_x_position-ball_radius >=screen_width):
          print("Game over, restarting...")
          pygame.time.delay(1000)
          ball_x_position=screen_width//2
          ball_y_position=screen_height//2
          print("hit")
          score =0
+        
+    if(hit):
+         score+=1
 
     screen.fill("black")
     pygame.draw.circle(screen,white,((ball_x_position),(ball_y_position)),ball_radius)
-    pygame.draw.rect(screen,white,((pad_y_position),(pad_x_position),pad_width,pad_height))
+    pygame.draw.rect(screen,white,((left_pad_y_position),(left_pad_x_position),pad_width,pad_height))
+    pygame.draw.rect(screen,white,((right_pad_y_position),(right_pad_x_position),pad_width,pad_height))
 
     text=font.render(f"Score: {score}",True,(255,255,255))
-    screen.blit(text,(screen_width//2,10))
-    if(hit==True):
-        displayScore()
-
+    text_rect=text.get_rect(center=(screen_width//2,20))
+    screen.blit(text,text_rect)
     
+
+    displayScore()
     pygame.display.flip()
     
     clock.tick(60)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-    screen.blit(text,(screen_width//2,10))
+if(ball_x_position-ball_radius<=0):
+    #      print("Game over, restarting...")
+    #      pygame.time.delay(1000)
+    #      ball_x_position=screen_width//2
+    #      ball_y_position=screen_height//2
+    #      print("hit")
+    #      score =0
